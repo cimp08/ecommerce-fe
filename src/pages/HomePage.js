1227a692ab3abc24a94af590312494a356d56153
Cemil Ülay <cemilulay@gmail.com>
2022-06-26 14:01:07 +0200

    filestructure for pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import products from '../products'
+
+// eslint-disable-next-line react/function-component-definition
+const HomePage = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <p>test</p>
+        </>
+    )
+}
+
+export default HomePage
