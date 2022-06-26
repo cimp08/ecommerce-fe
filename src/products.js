791f2d6318b62ff1af764e0350c8ff8cfaed574d
Cemil Ülay <cemilulay@gmail.com>
2022-06-26 14:01:20 +0200

    add products.js

---
+++ src/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Glasskydd iPhone 13 & 13 Pro',
+        image: '/images/apple-iphone-1313-pro.webp',
+        description:
+            'Skydda skärmen på din Apple iPhone 13 eller 13 Pro med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
+        brand: 'Apple',
+        category: 'Screenprotector',
+        price: 69,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '2',
+        name: 'Glasskydd iPhone 13 Mini',
+        image: '/images/apple-iphone-13-mini.webp',
+        description:
+            'Skydda skärmen på din Apple iPhone 13 mini med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
+        brand: 'Apple',
+        category: 'Screenprotector',
+        price: 69,
+        countInStock: 7,
+        rating: 4.0,
+        numReviews: 8,
+    },
+    {
+        _id: '3',
+        name: 'Glasskydd iPhone 13 Pro Max',
+        image: '/images/apple-iphone-13-pro-max.webp',
+        description:
+            'Skydda skärmen på din Apple iPhone 13 Pro Max med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
+        brand: 'Apple',
+        category: 'Screenprotector',
+        price: 69,
+        countInStock: 5,
+        rating: 3,
+        numReviews: 12,
+    },
+    {
+        _id: '4',
+        name: 'Glasskydd iPhone 12 Pro Max',
+        image: '/images/apple-iphone-12-pro-max.webp',
+        description:
+            'Skydda skärmen på din Apple iPhone 12 Pro Max med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
+        brand: 'Apple',
+        category: 'Screenprotector',
+        price: 69,
+        countInStock: 11,
+        rating: 5,
+        numReviews: 12,
+    },
+    {
+        _id: '5',
+        name: 'Glasskydd iPhone X/XS/11 Pro',
+        image: '/images/apple-iphone-xxs11.webp',
+        description:
+            'Skydda skärmen på din Apple iPhone X/XS/11 Pro med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
+        brand: 'Apple',
+        category: 'Screenprotector',
+        price: 69,
+        countInStock: 7,
+        rating: 3.5,
+        numReviews: 10,
+    },
+    {
+        _id: '6',
+        name: 'Glasskydd iPhone 6/6S/7/8/SE',
+        image: '/images/apple-iphone-xxs11.webp',
+        description:
+            'Skydda skärmen på din Apple iPhone 6/6S/7/8/SE Pro med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
+        brand: 'Apple',
+        category: 'Screenprotector',
+        price: 69,
+        countInStock: 0,
+        rating: 4,
+        numReviews: 12,
+    },
+]
+
+export default products
