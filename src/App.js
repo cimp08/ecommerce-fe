7fff53b81e351997c9b6f9f4c7cb55938d2d8ec4
Cemil Ülay <cemilulay@gmail.com>
2022-06-27 12:53:17 +0200

    add hero component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css'
 import React from 'react'
 import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
 import Footer from './components/footer/Footer'
 
 // eslint-disable-next-line react/function-component-definition
 const App = () => {
     return (
         <>
+            <Header />
             <main>
-                <Header />
-                <h1>MAIN</h1>
+                <Hero />
             </main>
             <Footer />
         </>
