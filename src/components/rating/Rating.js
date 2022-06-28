f74379d7175884cf705acd777817643962d4117a
Cemil Ülay <cemilulay@gmail.com>
2022-06-28 23:50:50 +0200

    add rating component and css

---
+++ src/components/rating/Rating.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-nested-ternary */
+import React from 'react'
+import { BsStar, BsStarHalf, BsStarFill } from 'react-icons/bs'
+import './rating.css'
+import PropTypes from 'prop-types'
+
+// eslint-disable-next-line react/function-component-definition
+const Rating = ({ value, text, color }) => {
+    return (
+        <div className="rating">
+            {value >= 1 ? (
+                <BsStarFill style={{ color }} />
+            ) : value >= 0.5 ? (
+                <BsStarHalf style={{ color }} />
+            ) : (
+                <BsStar style={{ color }} />
+            )}
+            {value >= 2 ? (
+                <BsStarFill style={{ color }} />
+            ) : value >= 1.5 ? (
+                <BsStarHalf style={{ color }} />
+            ) : (
+                <BsStar style={{ color }} />
+            )}
+            {value >= 3 ? (
+                <BsStarFill style={{ color }} />
+            ) : value >= 2.5 ? (
+                <BsStarHalf style={{ color }} />
+            ) : (
+                <BsStar style={{ color }} />
+            )}
+            {value >= 4 ? (
+                <BsStarFill style={{ color }} />
+            ) : value >= 3.5 ? (
+                <BsStarHalf style={{ color }} />
+            ) : (
+                <BsStar style={{ color }} />
+            )}
+            {value >= 5 ? (
+                <BsStarFill style={{ color }} />
+            ) : value >= 4.5 ? (
+                <BsStarHalf style={{ color }} />
+            ) : (
+                <BsStar style={{ color }} />
+            )}
+            <p>{text && text}</p>
+        </div>
+    )
+}
+
+Rating.defaultProps = {
+    color: '#828282',
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
+}
+
+export default Rating
