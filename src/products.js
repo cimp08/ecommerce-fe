fffed1c5c6eed15a50d21836c0b770a7e1de8685
Cemil Ülay <cemilulay@gmail.com>
2022-06-28 20:15:43 +0200

    add map for products

--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 const products = [
     {
         _id: '1',
-        name: 'Glasskydd iPhone 13 & 13 Pro',
+        name: 'iPhone 13 & 13 Pro',
         image: '/images/apple-iphone-1313-pro.webp',
         description:
             'Skydda skärmen på din Apple iPhone 13 eller 13 Pro med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
@@ -14,8 +14,8 @@ const products = [
     },
     {
         _id: '2',
-        name: 'Glasskydd iPhone 13 Mini',
-        image: '/images/apple-iphone-13-mini.webp',
+        name: 'iPhone 13 Mini',
+        image: '/images/apple-iphone-xxs11.webp',
         description:
             'Skydda skärmen på din Apple iPhone 13 mini med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
         brand: 'Apple',
@@ -27,8 +27,8 @@ const products = [
     },
     {
         _id: '3',
-        name: 'Glasskydd iPhone 13 Pro Max',
-        image: '/images/apple-iphone-13-pro-max.webp',
+        name: 'iPhone 13 Pro Max',
+        image: '/images/apple-iphone-xxs11.webp',
         description:
             'Skydda skärmen på din Apple iPhone 13 Pro Max med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
         brand: 'Apple',
@@ -40,8 +40,8 @@ const products = [
     },
     {
         _id: '4',
-        name: 'Glasskydd iPhone 12 Pro Max',
-        image: '/images/apple-iphone-12-pro-max.webp',
+        name: 'iPhone 12 Pro Max',
+        image: '/images/apple-iphone-xxs11.webp',
         description:
             'Skydda skärmen på din Apple iPhone 12 Pro Max med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
         brand: 'Apple',
@@ -53,7 +53,7 @@ const products = [
     },
     {
         _id: '5',
-        name: 'Glasskydd iPhone X/XS/11 Pro',
+        name: 'iPhone X/XS/11 Pro',
         image: '/images/apple-iphone-xxs11.webp',
         description:
             'Skydda skärmen på din Apple iPhone X/XS/11 Pro med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
@@ -66,7 +66,7 @@ const products = [
     },
     {
         _id: '6',
-        name: 'Glasskydd iPhone 6/6S/7/8/SE',
+        name: 'iPhone 6/6S/7/8/SE',
         image: '/images/apple-iphone-xxs11.webp',
         description:
             'Skydda skärmen på din Apple iPhone 6/6S/7/8/SE Pro med glass screen protector. Högkvalitativt 9H härdat glas och ger din mobil ett heltäckande kant-till-kant-skydd som skyddar displayen mot repor, sprickor, stötar och slag. Självklart håller skyddet en hög klarhet med full transparens och full touch-funktionalitet av pekskärmen. ',
