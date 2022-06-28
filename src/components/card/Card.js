0154331af50d7867b789bbff8975d977be978269
Cemil Ülay <cemilulay@gmail.com>
2022-06-28 20:19:20 +0200

    add product props

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react'
 import './card.css'
-import IMG from '../../assets/img/apple-iphone-xxs11.webp'
 
 // eslint-disable-next-line react/function-component-definition
-const Card = () => {
+const Card = ({ product }) => {
     return (
         <div className="card__content">
             <div className="card__content-img">
-                <img src={IMG} alt="screen protector" />
+                <img src={product.image} alt="screen protector" />
             </div>
-            <p className="card__content-brand">Apple</p>
-            <p className="card__content-model">iphone 13 pro -5g</p>
-            <p className="card__content-price">Pris 69:-</p>
-            <a href="/">lägg till</a>
+            <p className="card__content-brand">{product.brand}</p>
+            <p className="card__content-model">{product.name}</p>
+            <p className="card__content-price">Pris {product.price}:-</p>
+            <p className="card__content-rating">
+                {product.rating} av {product.numReviews}
+            </p>
+            <a href={`/products/${product._id}`}>info</a>
         </div>
     )
 }
