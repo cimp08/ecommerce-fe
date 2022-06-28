6afcb393952273d0945ee9b275c1319a0b6410ce
Cemil Ülay <cemilulay@gmail.com>
2022-06-28 23:49:41 +0200

    import Rating component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react'
 import './card.css'
+import Rating from '../rating/Rating'
 
 // eslint-disable-next-line react/function-component-definition
 const Card = ({ product }) => {
@@ -12,9 +13,7 @@ const Card = ({ product }) => {
             <p className="card__content-brand">{product.brand}</p>
             <p className="card__content-model">{product.name}</p>
             <p className="card__content-price">Pris {product.price}:-</p>
-            <p className="card__content-rating">
-                {product.rating} av {product.numReviews}
-            </p>
+            <Rating value={product.rating} text={`${product.numReviews} recensioner`} />
             <a href={`/products/${product._id}`}>info</a>
         </div>
     )
