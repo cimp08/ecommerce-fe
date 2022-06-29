dc463072f1fb654292ba1b7b7153e6331bf0eb22
Cemil Ülay <cemilulay@gmail.com>
2022-06-29 12:52:12 +0200

    create ProductPage

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// eslint-disable-next-line react/function-component-definition
+const ProductPage = () => {
+    return <div>Product</div>
+}
+
+export default ProductPage
