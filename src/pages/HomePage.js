e7b358eef3d95273cd90fdd204f96dfa563135ae
Cemil Ülay <cemilulay@gmail.com>
2022-06-29 12:51:53 +0200

    import hero, featured, products components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import products from '../products'
+import Hero from '../components/hero/Hero'
+import Featured from '../components/featured/Featured'
+import Products from '../components/products/Products'
 
 // eslint-disable-next-line react/function-component-definition
 const HomePage = () => {
     return (
         <>
-            <h1>Latest Products</h1>
-            <p>test</p>
+            <Hero />
+            <Featured />
+            <Products />
         </>
     )
 }
