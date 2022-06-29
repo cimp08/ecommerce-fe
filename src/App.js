ce94443f76034fbaf059b485a5479c90d99fec17
Cemil Ülay <cemilulay@gmail.com>
2022-06-29 12:50:55 +0200

    import BrowserRouter, Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
-import Hero from './components/hero/Hero'
-import Featured from './components/featured/Featured'
-import Products from './components/products/Products'
 import Footer from './components/footer/Footer'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 // eslint-disable-next-line react/function-component-definition
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main>
-                <Hero />
-                <Featured />
-                <Products />
+                <Routes>
+                    <Route path="/" element={<HomePage />} exact />
+                    <Route path="/product/:id" element={<ProductPage />} />
+                </Routes>
             </main>
             <Footer />
-        </>
+        </Router>
     )
 }
 
