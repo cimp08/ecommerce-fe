8232c6669d88c6160345df66fdc1a43e0b7501f7
Cemil Ülay <cemilulay@gmail.com>
2022-06-29 22:05:19 +0200

    import ProductDetails component

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import ProductDetails from '../components/productDetails/ProductDetails'
 
 // eslint-disable-next-line react/function-component-definition
 const ProductPage = () => {
-    return <div>Product</div>
+    return <ProductDetails />
 }
 
 export default ProductPage
