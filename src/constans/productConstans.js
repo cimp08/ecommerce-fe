4b764fcea8d47bc487f9531ecccd9ef5a56a9807
Cemil Ülay <cemilulay@gmail.com>
2022-07-02 11:10:00 +0200

    add constans for product

---
+++ src/constans/productConstans.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST'
+export const PRODUCT_LIST_SUCCESS = 'PRODUCT_LIST_SUCCESS'
+export const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL'
