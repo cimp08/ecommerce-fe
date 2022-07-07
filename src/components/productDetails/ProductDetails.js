8df2fdfcef842c86672580be4934f7939c027463
Cemil Ülay <cemilulay@gmail.com>
2022-07-07 21:46:40 +0200

    import loader, message, listProductDetails, useDispatch, useSelector

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -1,27 +1,29 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-underscore-dangle */
-import React, { useState, useEffect } from 'react'
+import React, { /* useState, */ useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { TbTruckDelivery } from 'react-icons/tb'
 import { BsArrowLeft } from 'react-icons/bs'
-import axios from 'axios'
-import Rating from '../rating/Rating'
+import { listProductDetails } from '../../actions/productActions'
+/* import Rating from '../rating/Rating' */
+import Loader from '../loader/Loader'
+import Message from '../message/Message'
 import './productDetails.css'
 
 // eslint-disable-next-line react/function-component-definition
 const ProductDetails = () => {
+    const dispatch = useDispatch()
+    const productDetails = useSelector((state) => state.productDetails)
+    const { loading, error, product } = productDetails
     // Get the id from the url
     const { id } = useParams()
-    const [product, setProduct] = useState({})
-    const [showMore, setShowMore] = useState(false)
+    /* const [showMore, setShowMore] = useState(false) */
 
     useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${id}`)
-            setProduct(data)
-        }
-        fetchProduct()
-    }, [id])
+        dispatch(listProductDetails(id))
+    }, [dispatch, id])
 
     return (
         <section className="product__section">
@@ -30,39 +32,52 @@ const ProductDetails = () => {
                     <BsArrowLeft className="back-icon" />
                     Tillbaka
                 </Link>
-                <div className="product__item">
-                    <div className="product__item-img">
-                        <img src={product.image} alt={product.name} />
-                    </div>
-                    <div className="product__item-info">
-                        <div className="price-rating">
-                            <p className="price">{product.price} kr</p>
-                            <Rating
-                                value={product.rating}
-                                text={`${product.numReviews} recensioner`}
-                            />
+                {loading ? (
+                    <Loader />
+                ) : error ? (
+                    <Message variant="danger">{error}</Message>
+                ) : (
+                    <>
+                        <div className="product__item">
+                            <div className="product__item-img">
+                                <img src={product.image} alt={product.name} />
+                            </div>
+                            <div className="product__item-info">
+                                <div className="price-rating">
+                                    <p className="price">{product.price} kr</p>
+                                    {/* <Rating
+                                        value={product.rating}
+                                        text={`${product.numReviews} recensioner`}
+                                    /> */}
+                                </div>
+                                <h3 className="name">Glasskydd 9HD- {product.name}</h3>
+                                <button className="btn" disabled={product.countInStock === 0}>
+                                    Köp
+                                </button>
+                                <div className="stock">
+                                    <TbTruckDelivery className="stock-icon" />
+                                    <p>
+                                        {product.countInStock > 0
+                                            ? 'Finns i lager !'
+                                            : 'Slut i lager !'}
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <h3 className="name">Glasskydd 9HD- {product.name}</h3>
-                        <button className="btn" disabled={product.countInStock === 0}>
-                            Köp
-                        </button>
-                        <div className="stock">
-                            <TbTruckDelivery className="stock-icon" />
-                            <p>{product.countInStock > 0 ? 'Finns i lager !' : 'Slut i lager !'}</p>
+                        <div className="product__description">
+                            <h3>Produktbeskrivning</h3>
+                            <p>{product.description}</p>
+                            {/* <p>
+                                {showMore
+                                    ? product.description
+                                    : `${product.description.substring(0, 100)}`}
+                            </p>
+                            <button className="btn-read" onClick={() => setShowMore(!showMore)}>
+                                {showMore ? 'Dölj' : 'Läs mer'}
+                            </button> */}
                         </div>
-                    </div>
-                </div>
-                <div className="product__description">
-                    <h3>Produktbeskrivning</h3>
-                    <p>
-                        {showMore
-                            ? product.description
-                            : `${product.description.substring(0, 100)}`}
-                    </p>
-                    <button className="btn-read" onClick={() => setShowMore(!showMore)}>
-                        {showMore ? 'Dölj' : 'Läs mer'}
-                    </button>
-                </div>
+                    </>
+                )}
             </div>
         </section>
     )
