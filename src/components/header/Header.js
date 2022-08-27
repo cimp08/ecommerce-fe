b8bdb7fe49aff1c130dfb4ac0232029c07eacb2a
Cemil Ülay <cemilulay@gmail.com>
2022-08-27 13:03:07 +0200

    add dropdown and show user in Navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import './header.css'
+import { LinkContainer } from 'react-router-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
 import LOGO from '../../assets/icons/shield.png'
 import SHOPPINGCART from '../../assets/icons/shopping-bag.png'
 import USER from '../../assets/icons/user.png'
+import { logout } from '../../actions/userAction'
 
 // eslint-disable-next-line react/function-component-definition
 const Header = () => {
+    // eslint-disable-next-line no-unused-vars
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <nav>
@@ -23,9 +37,18 @@ const Header = () => {
                         <Link to="/cart">
                             <img src={SHOPPINGCART} alt="shoppingcart icon" />
                         </Link>
-                        <Link to="/login">
-                            <img src={USER} alt="user icon" />
-                        </Link>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id="username" className="dropdown">
+                                <LinkContainer to="/profile">
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <Link to="/login">
+                                <img src={USER} alt="user icon" />
+                            </Link>
+                        )}
                     </div>
                 </div>
             </nav>
