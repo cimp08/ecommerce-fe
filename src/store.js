71862d870df685ace3453377c288de25968cca86
Cemil Ülay <cemilulay@gmail.com>
2022-08-28 08:45:56 +0200

    import userDetails, userUpdateProfile Reducers

--- src/store.js
+++ src/store.js
@@ -4,7 +4,12 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userUpdateProfileReducer,
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -12,6 +17,8 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
