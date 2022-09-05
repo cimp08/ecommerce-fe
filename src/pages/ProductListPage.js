17bf4b6fa441b155a37f1acfb14ee2a8437c66b2
Cemil Ülay <cemilulay@gmail.com>
2022-09-05 18:08:36 +0200

    add ProductListPage and list the products in table

---
+++ src/pages/ProductListPage.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-nested-ternary */
+import React, { useEffect } from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { BiEdit } from 'react-icons/bi'
+import { BsPlus } from 'react-icons/bs'
+import { FaTrash } from 'react-icons/fa'
+import Message from '../components/message/Message'
+import Loader from '../components/loader/Loader'
+import { listProducts } from '../actions/productActions'
+import './userListPage.css'
+
+// eslint-disable-next-line react/function-component-definition
+const ProductListPage = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products } = productList
+
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
+    useEffect(() => {
+        if (userInfo && userInfo.isAdmin) {
+            dispatch(listProducts())
+        } else {
+            navigate('/login')
+        }
+    }, [dispatch, navigate, userInfo])
+
+    const deleteHandler = () => {
+        // eslint-disable-next-line no-alert, no-undef
+        if (window.confirm('Ta bort användare?')) {
+            // Del prod
+        }
+    }
+
+    const createProductHandler = () => {
+        // Create product
+    }
+
+    return (
+        <div className="container userlist__section">
+            <Row className="align-items-center">
+                <Col>
+                    <h2>Products</h2>
+                </Col>
+                <Col className="text-right">
+                    <Button className="my-3" onClick={createProductHandler}>
+                        <BsPlus /> Create Product
+                    </Button>
+                </Col>
+            </Row>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <Table striped bordered hover responsive className="table-sm">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>MODELL</th>
+                            <th>PRIS</th>
+                            <th>KATEGORI</th>
+                            <th>MÄRKE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product) => (
+                            <tr key={product._id}>
+                                <td>{product._id}</td>
+                                <td>{product.name}</td>
+                                <td>{product.price} kr</td>
+                                <td>{product.category}</td>
+                                <td>{product.brand}</td>
+                                <td>
+                                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                        <Button variant="light" className="btn-lg">
+                                            <BiEdit />
+                                        </Button>
+                                    </LinkContainer>
+                                    <Button
+                                        variant="danger"
+                                        className="btn-lg"
+                                        onClick={() => {
+                                            deleteHandler(product._id)
+                                        }}
+                                    >
+                                        <FaTrash />
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </div>
+    )
+}
+
+export default ProductListPage
