09b1be0581213a701e4b3dffd0aa9457f5968f9e
Cemil Ülay <cemilulay@gmail.com>
2022-09-07 08:26:22 +0200

    dont show loading text

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -9,7 +9,7 @@ const Loader = () => {
             role="status"
             style={{ width: '100px', height: '100px', margin: 'auto', display: 'block' }}
         >
-            <span className="sr-only">Loading...</span>
+            <span className="visually-hidden">Loading...</span>
         </Spinner>
     )
 }
