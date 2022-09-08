b7bf866080785a90440365edc84a9ceda3b6b7a3
Cemil Ülay <cemilulay@gmail.com>
2022-09-08 11:06:13 +0200

    add orderListReducer and orderDeliverReducer

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -15,6 +15,13 @@ import {
     ORDER_MY_LIST_SUCCESS,
     ORDER_MY_LIST_FAIL,
     ORDER_MY_LIST_RESET,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+    ORDER_LIST_FAIL,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET,
 } from '../constans/orderConstans'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -87,6 +94,29 @@ export const orderPayReducer = (state = {}, action) => {
     }
 }
 
+export const orderDeliverReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_DELIVER_REQUEST:
+            return {
+                loading: true,
+            }
+        case ORDER_DELIVER_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+            }
+        case ORDER_DELIVER_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        case ORDER_DELIVER_RESET:
+            return {}
+        default:
+            return state
+    }
+}
+
 export const orderMyListReducer = (state = { orders: [] }, action) => {
     switch (action.type) {
         case ORDER_MY_LIST_REQUEST:
@@ -105,6 +135,27 @@ export const orderMyListReducer = (state = { orders: [] }, action) => {
             }
         case ORDER_MY_LIST_RESET:
             return { orders: [] }
+        default:
+            return state
+    }
+}
+
+export const orderListReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+        case ORDER_LIST_REQUEST:
+            return {
+                loading: true,
+            }
+        case ORDER_LIST_SUCCESS:
+            return {
+                loading: false,
+                orders: action.payload,
+            }
+        case ORDER_LIST_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
         default:
             return state
     }
