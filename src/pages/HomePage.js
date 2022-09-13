2e0342216c8a429c88245ffdbd32b4b416451baa
Cemil Ülay <cemilulay@gmail.com>
2022-09-13 19:26:57 +0200

    display components based on keyword if a used the searchbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import Hero from '../components/hero/Hero'
 import Featured from '../components/featured/Featured'
 import Products from '../components/products/Products'
 
 // eslint-disable-next-line react/function-component-definition
 const HomePage = () => {
+    const { keyword } = useParams()
     return (
         <>
-            <Hero />
-            <Featured />
-            <Products />
+            {keyword && (
+                <>
+                    <Products />
+                    <Featured />
+                </>
+            )}
+
+            {!keyword && (
+                <>
+                    <Hero />
+                    <Featured />
+                    <Products />
+                </>
+            )}
         </>
     )
 }
