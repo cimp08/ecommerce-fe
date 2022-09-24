fa8c4163dfba1f1a62e80639bd1aa7830b5861c2
Cemil Ülay <cemilulay@gmail.com>
2022-09-24 11:55:02 +0200

    update css and responsivity

--- src/pages/OrderListPage.js
+++ src/pages/OrderListPage.js
@@ -10,7 +10,7 @@ import { BsXLg } from 'react-icons/bs'
 import Message from '../components/message/Message'
 import Loader from '../components/loader/Loader'
 import { listOrders } from '../actions/orderActions'
-import './userListPage.css'
+import './orderListPage.css'
 
 // eslint-disable-next-line react/function-component-definition
 const OrderListPage = () => {
@@ -32,71 +32,73 @@ const OrderListPage = () => {
     }, [dispatch, navigate, userInfo])
 
     return (
-        <div className="container userlist__section">
-            <h2>Ordrar</h2>
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <Message variant="danger">{error}</Message>
-            ) : (
-                <Table striped bordered hover responsive className="table-sm">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>ANVÄNDARE</th>
-                            <th>DATUM</th>
-                            <th>TOTALT</th>
-                            <th>BETALD</th>
-                            <th>LEVERERAD</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orders.map((order) => (
-                            <tr key={order._id}>
-                                <td>{order._id}</td>
-                                <td>{order.user && order.user.name}</td>
-                                <td>{order.createdAt.substring(0, 10)}</td>
-                                <td>{order.totalPrice} kr</td>
-                                <td>
-                                    {order.isPaid ? (
-                                        order.paidAt.substring(0, 10)
-                                    ) : (
-                                        <IconContext.Provider
-                                            // eslint-disable-next-line react/jsx-no-constructed-context-values
-                                            value={{
-                                                color: 'red',
-                                            }}
-                                        >
-                                            <BsXLg />
-                                        </IconContext.Provider>
-                                    )}
-                                </td>
-                                <td>
-                                    {order.isDelivered ? (
-                                        order.deliveredAt.substring(0, 10)
-                                    ) : (
-                                        <IconContext.Provider
-                                            // eslint-disable-next-line react/jsx-no-constructed-context-values
-                                            value={{
-                                                color: 'red',
-                                            }}
-                                        >
-                                            <BsXLg />
-                                        </IconContext.Provider>
-                                    )}
-                                </td>
-                                <td>
-                                    <LinkContainer to={`/order/${order._id}`}>
-                                        <Button variant="light" className="btn-lg">
-                                            Detaljer
-                                        </Button>
-                                    </LinkContainer>
-                                </td>
+        <div className="section-orderlist">
+            <div className="orderlist">
+                <h2 className="mb-5">Ordrar</h2>
+                {loading ? <Loader /> : null}
+                {error ? <Message variant="danger">{error}</Message> : null}
+                {orders && orders.length ? (
+                    <Table striped bordered hover responsive className="table-sm">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>ANVÄNDARE</th>
+                                <th>DATUM</th>
+                                <th>TOTALT</th>
+                                <th>BETALD</th>
+                                <th>LEVERERAD</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            )}
+                        </thead>
+                        <tbody>
+                            {orders.map((order) => (
+                                <tr key={order._id}>
+                                    <td>{order._id}</td>
+                                    <td>{order.user && order.user.name}</td>
+                                    <td className="date">{order.createdAt.substring(0, 10)}</td>
+                                    <td>{order.totalPrice} kr</td>
+                                    <td>
+                                        {order.isPaid ? (
+                                            order.paidAt.substring(0, 10)
+                                        ) : (
+                                            <IconContext.Provider
+                                                // eslint-disable-next-line react/jsx-no-constructed-context-values
+                                                value={{
+                                                    color: 'red',
+                                                }}
+                                            >
+                                                <BsXLg />
+                                            </IconContext.Provider>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {order.isDelivered ? (
+                                            order.deliveredAt.substring(0, 10)
+                                        ) : (
+                                            <IconContext.Provider
+                                                // eslint-disable-next-line react/jsx-no-constructed-context-values
+                                                value={{
+                                                    color: 'red',
+                                                }}
+                                            >
+                                                <BsXLg />
+                                            </IconContext.Provider>
+                                        )}
+                                    </td>
+                                    <td>
+                                        <LinkContainer to={`/order/${order._id}`}>
+                                            <Button variant="light" className="btn-lg">
+                                                Detaljer
+                                            </Button>
+                                        </LinkContainer>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                ) : (
+                    <Message variant="danger">Finns inga ordrar</Message>
+                )}
+            </div>
         </div>
     )
 }
