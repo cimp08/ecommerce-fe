70406ee5eac157516e3946cb6632f3d1fdb64f05
Cemil Ülay <cemilulay@gmail.com>
2022-10-31 18:25:22 +0100

    fix after update a product, dispatch product details success to display updated data

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -176,6 +176,11 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
             type: USER_UPDATE_PROFILE_SUCCESS,
             payload: data,
         })
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
     } catch (error) {
         dispatch({
             type: USER_UPDATE_PROFILE_FAIL,
@@ -318,6 +323,7 @@ export const updateUser = (user) => async (dispatch, getState) => {
             type: USER_DETAILS_SUCCESS,
             payload: data,
         })
+        dispatch({ type: USER_DETAILS_RESET })
     } catch (error) {
         dispatch({
             type: USER_UPDATE_FAIL,
